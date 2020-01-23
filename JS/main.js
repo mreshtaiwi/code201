e066c8cfae2e2c9f11b00f36e65a266bc19e3c17
abualrubbaraa <baraaabualrub1998@gmail.com>
2020-01-23 14:32:29 +0200

    lab 03

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ function welcome() {
         alert("okay, welcome " + client_name + ".. now let's enojy :)");
 
 }
-function Game() {
+function Lab02() {
     var Questions = ["Is Amman the capital of Joradn?", "Do you think my name is Baraa?", "1 + 2 is 4?", "The cat have 2 legs and 2 hands?", "Baraa is a female name only :)"];
     var Answers = [true, true, false, true, false];
     var res = [];
@@ -28,5 +28,66 @@ function Game() {
             client_name + " , on the left side you can see your pricture :) Also, you got " + total_result + "/5 :) ";
     }
 }
-welcome();
-Game();
+
+function Lab03_a(){
+    my_month=3;
+    numberOfTries =0; 
+    alert("Guess Game, 4 attemps , choose a number from 1-12 to know in which month i was born :) Let us start ....");
+    
+    for(var i=0;i<4;i++){
+        var answer = prompt("Choose a number between (1-12)");
+        numberOfTries++;
+        // input check :)
+        while(!Number(answer) || answer>12 || answer<1){
+            answer = prompt("please, NUMBER between 1-12");
+        }
+
+        // processing 
+        answer = my_month - answer;
+        
+        if(answer==0){
+            alert("You did it :) from the "+numberOfTries+" try.");
+            return true;
+        }
+        else if(answer>0){
+            if(answer > 2)
+                alert("Nah, too low");
+            else
+                alert("so close");
+        }
+        else{
+            if(Math.abs(answer) > 2)
+                alert("Nah, too low");
+            else
+                alert("so close");
+        }
+    }
+    alert("bad");
+}
+function Lab03_b(){
+    var lab_question = "What are my brothers names?";
+    var answers_for_lab03 = ["bahaa","alaa","mustafa"];
+
+    alert("Guess the answer fo this question, i have three you have 6 attempts:) lets go");
+    var answer = prompt(lab_question);
+    var j = 1; 
+
+    for(j;j<6;j++){
+        for(var i=0;i<3;i++)
+            if(answers_for_lab03[i]===answer.toLocaleLowerCase())
+                {
+                    alert("good job, you did it from the"+j +" try");
+                    return true;
+                }
+        alert("Wrong! you still have"+ (6-j) + " attemps..");
+        answer = prompt(lab_question);
+    }   
+    alert("BAAAAAAAAAAAAAAAAAAAAD, the answers are :- "+answers_for_lab03);
+            
+}
+
+//welcome();
+//Lab02();
+//Lab03_a();
+//Lab03_b()
+
